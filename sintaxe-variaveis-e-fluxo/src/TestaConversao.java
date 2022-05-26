545bbbeb5e83aff7c5bbabfa4cc618a9d803996a
Paulo Rubens <paulorubensbarbosa@outlook.com>
2022-05-25 22:29:23 -0300

    classe TestaConversao modificada

--- sintaxe-variaveis-e-fluxo/src/TestaConversao.java
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -3,6 +3,9 @@ public class TestaConversao {
 
 	public static void main(String[] args) {
 		double salario = 1270.50;
+		
+		
+		float pontoFlutuante = 3.14f;
 		int valor = (int) salario;
 		System.out.println(valor);
 		
